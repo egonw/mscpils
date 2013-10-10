0d2e267b412de4290df32893519e70d6cdd97d68
Egon Willighagen <egon.willighagen@gmail.com>
2013-10-10 22:26:50 +0200

    A first example

---
+++ README.md
@@ -0,0 +1,4 @@
+= Programming for the Life Sciences =
+
+These examples make use of ops.js by Ian Dunlop, available from http://github.com/openphacts/.
+
---
+++ src/PathwaySearch.js
@@ -0,0 +1,50 @@
+Openphacts.PathwaySearch = function PathwaySearch(baseURL, appID, appKey) {
+	this.baseURL = baseURL;
+	this.appID = appID;
+	this.appKey = appKey;
+}
+
+Openphacts.PathwaySearch.prototype.getInformation = function(callback, uri) {
+	var activityQuery = $.ajax({
+		url: this.baseURL + '/pathway',
+                dataType: 'json',
+		cache: true,
+		data: {
+			_format: "json",
+			app_id: this.appID,
+			app_key: this.appKey,
+                        uri: uri
+		},
+		success: function(response, status, request) {
+			callback.call(this, true, request.status, response.result.primaryTopic);
+		},
+		error: function(request, status, error) {
+			callback.call(this, false, request.status);
+		}
+	});
+}
+
+Openphacts.PathwaySearch.prototype.parseInformation = function(response) {
+        var identifier, title, description, inDataset, ontology, organism, organismLabel;
+        identifier = response.identifier;
+        title = response.title;
+        description = response.description;
+        inDataset = response.inDataset;
+        ontology = response.pathwayOntology;
+        organism = response.organism ? response.organism["_about"] : null;
+        organismLabel = response.organism ? response.organism.label : null;
+        var parts = [];
+	$.each(response.hasPart, function(i, part) {
+            parts.push({about: part["_about"], type: part.type});
+	});
+	return {
+                   identifier: identifier, 
+                   title: title, 
+                   description: description, 
+                   inDataset: inDataset, 
+                   ontology: ontology,
+                   organism: organism, 
+                   organismLabel: organismLabel, 
+                   parts: parts
+                };
+}
