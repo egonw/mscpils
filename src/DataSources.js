9e682edd514aefc21aec11265afbda8d83d07851
Egon Willighagen <egon.willighagen@gmail.com>
2013-11-06 22:47:44 +0100

    Added my own made DataSources.js

---
+++ src/DataSources.js
@@ -0,0 +1,25 @@
+Openphacts.DataSources = function DataSources(baseURL, appID, appKey) {
+        this.baseURL = baseURL;
+        this.appID = appID;
+        this.appKey = appKey;
+}
+
+Openphacts.DataSources.prototype.getSources = function(callback) {
+        var sourcesQuery = $.ajax({
+                url: this.baseURL + '/sources',
+                dataType: 'json',
+                cache: true,
+                data: {
+                        _format: "json",
+                        app_id: this.appID,
+                        app_key: this.appKey
+                },
+                success: function(response, status, request) {
+                        callback.call(this, true, request.status, response.result.primaryTopic);
+                },
+                error: function(request, status, error) {
+                        callback.call(this, false, request.status);
+                }
+        });
+}
+
